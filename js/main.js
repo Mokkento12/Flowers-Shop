55f087a1257cdcba2b18c83f52ff6a592b18d5f4
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-08-05 10:38:59 +0300

    create: slider for flowers

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const flowersSlider = new Swiper(".flowers-slider", {
+  // Параметры слайдера
+
+  loop: true,
+  slidesPerView: 6,
+
+  // Стрелки
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
