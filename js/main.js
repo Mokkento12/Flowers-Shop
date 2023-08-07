7f9517c205865cafad02040a9001f6d87c3c8e66
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-08-07 10:21:46 +0300

    create: block advantages & reviews

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ const flowersSlider = new Swiper(".flowers-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Параметры слайдера
+
+  loop: true,
+  slidesPerView: 1,
+
+  // Стрелки
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
