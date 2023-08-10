c96677a1ef49a4d32873416794c701dc06b18ea7
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-08-10 13:08:47 +0300

    create: responsive

--- js/main.js
+++ js/main.js
@@ -9,6 +9,22 @@ const flowersSlider = new Swiper(".flowers-slider", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 6,
+    },
+  },
 });
 
 const reviewsSlider = new Swiper(".reviews-slider", {
