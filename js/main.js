add0a7ea3500b535b76e2bb0168fc9c563400bd3
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-08-10 18:21:54 +0300

    create: modal window & funcybox gallery

--- js/main.js
+++ js/main.js
@@ -1,41 +1,84 @@
-const flowersSlider = new Swiper(".flowers-slider", {
-  // Параметры слайдера
+$(document).ready(function () {
+  const flowersSlider = new Swiper(".flowers-slider", {
+    // Параметры слайдера
 
-  loop: true,
-  slidesPerView: 6,
+    loop: true,
+    slidesPerView: 6,
 
-  // Стрелки
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
+    // Стрелки
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
+
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 6,
+      },
     },
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 6,
+  });
+
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Параметры слайдера
+
+    loop: true,
+    slidesPerView: 1,
+
+    // Стрелки
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-  },
-});
+  });
 
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Параметры слайдера
-
-  loop: true,
-  slidesPerView: 1,
-
-  // Стрелки
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  $("#review-1").on("click", function () {
+    $.fancybox.open(
+      [
+        {
+          src: "../img/flowers/bouquet-1.jpg",
+          opts: {
+            caption: "First caption",
+            thumb: "../img/flowers/bouquet-1.jpg",
+          },
+        },
+        {
+          src: "../img/flowers/bouquet-2.jpg",
+          opts: {
+            caption: "Second caption",
+            thumb: "../img/flowers/bouquet-2.jpg",
+          },
+        },
+        {
+          src: "../img/flowers/bouquet-3.jpg",
+          opts: {
+            caption: "Second caption",
+            thumb: "../img/flowers/bouquet-3.jpg",
+          },
+        },
+        {
+          src: "../img/flowers/bouquet-4.jpg",
+          opts: {
+            caption: "Second caption",
+            thumb: "../img/flowers/bouquet-4.jpg",
+          },
+        },
+      ],
+      {
+        loop: true,
+        thumbs: {
+          autoStart: true,
+        },
+      }
+    );
+  });
 });
